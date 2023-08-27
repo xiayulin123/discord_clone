de5b3d841ef5dfc7519fcbced1895f852b90611e
xiayulin123 <xiayulinsky@gmail.com>
2023-08-27 10:27:38 -0400

    editing page for server

---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,42 @@
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+import { ChannelType } from "@prisma/client"
+import { redirect } from "next/navigation"
+
+interface ServerSidebarProps {
+    serverId : string
+}
+
+export const ServerSidebar = async ( {serverId} : ServerSidebarProps) => {
+    const profile = await currentProfile()
+    if(!profile) { 
+        return redirect("/")
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id:serverId
+        },
+        include: {
+            channels: {
+                orderBy: {
+                    createdAt: "asc",
+                }
+            },
+            members : {
+                include : {
+                    profile:true
+                },
+                orderBy: {
+                    role: "asc"
+                }
+            }
+        }
+    })
+    const textChannels = server?.channels.filter((channel)=> channel.type === ChannelType.TEXT)
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
