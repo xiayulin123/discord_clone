c070055f5c9511627cf63094a010d260af16510c
xiayulin123 <xiayulinsky@gmail.com>
2023-08-27 19:35:07 -0400

    finish function for server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -26,3 +26,24 @@ export async function PATCH(req:Request, {params}:{params: {serverId: string}}){
         return new NextResponse("Internal Error", {status: 500})
     }
 }
+
+export async function DELETE(req:Request, {params}:{params: {serverId: string}}){
+
+    try {
+        const profile = await currentProfile()
+        if(!profile){
+            return new NextResponse("Unauthorized", {status: 401})
+        }
+        const server = await db.server.delete({
+            where:{
+                id: params.serverId,
+                profileId: profile.id
+            },
+            
+        })
+        return NextResponse.json(server)
+    } catch (error) {
+        console.log("[ServerId Error", error)
+        return new NextResponse("Internal Error", {status: 500})
+    }
+}
