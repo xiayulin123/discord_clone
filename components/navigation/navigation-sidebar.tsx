441986133699b8bae11f8bf818ebd14954627396
xiayulin123 <xiayulinsky@gmail.com>
2023-08-26 22:18:27 -0400

    editing page for server

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation"
 import { NavigationAction } from "./navigation-action"
 import { Separator } from "@/components/ui/separator"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import { NavigationItem } from "./navigation-item"
 
 export const NavigationSidebar = async ()=> {
     const profile = await currentProfile()
@@ -25,8 +26,12 @@ export const NavigationSidebar = async ()=> {
             <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server)=> (
-                    <div key={server.id}>
-                        {server.name}
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                        id={server.id}
+                        name={server.name}
+                        imageUrl={server.imageUrl}
+                        />
                     </div>
 
                 ))}
