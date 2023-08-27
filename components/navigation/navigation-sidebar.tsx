67fb5ebf7f8263c55f82435646be262b1a872f2d
xiayulin123 <xiayulinsky@gmail.com>
2023-08-26 22:09:31 -0400

    editing page for server

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,6 +1,7 @@
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { redirect } from "next/navigation"
+import { NavigationAction } from "./navigation-action"
 
 export const NavigationSidebar = async ()=> {
     const profile = await currentProfile()
@@ -18,7 +19,7 @@ export const NavigationSidebar = async ()=> {
     })
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-            sidebar
+            <NavigationAction />
         </div>
     )
 }
