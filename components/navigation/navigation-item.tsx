31e6ded70ef8f72202beecae7adc3ac2a1bbd80e
xiayulin123 <xiayulinsky@gmail.com>
2023-08-26 22:24:24 -0400

    editing page for server

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -14,11 +14,17 @@ interface NavigationItemProps {
 export const NavigationItem = ({
     id, imageUrl, name
 }:NavigationItemProps) => {
+    const params = useParams()
+    const router = useRouter()
     return (
         <ActionTooltip side="right" align="center" label={name}>
-            <div>
-                Server
-            </div>
+            <button onClick={()=>{}} className="group relative flex items-center">
+                <div className={cn("absolute left-0 bg-primary rounded-r-full transition-all w-[4px]", params?.serverId !== id && "group-hover:h-[20px]", params?.serverId ===id ? "h-[36px]" : "h-[8px]")} />
+                <div className={cn("relative group flex mx-3 h-[48px] rounded-[24px] groupd-hover:rounded-[16px] transition-all overflow-hidden" 
+                , params ?.serverId === id && "bg-primary/10 text-primary rounded-[16px]" )}>
+                    
+                </div>
+            </button>
         </ActionTooltip>
     )
 }
