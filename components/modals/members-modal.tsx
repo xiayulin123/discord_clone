2e8a10af9dfa69ead2adb6167fbf56d532c04b6e
xiayulin123 <xiayulinsky@gmail.com>
2023-08-27 18:52:02 -0400

    editing page for server

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -44,13 +44,33 @@ export const MembersModal = () => {
 
     const isModalOpen = isOpen && type === "members"
     const {server} = data as {server: ServerWithMembersWithProfiles}
+
+
+    const onKick = async (memberId: string) => {
+        try {
+            setLoadingId(memberId)
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server.id
+                }
+            })
+            const response = await axios.delete(url)
+            router.refresh()
+            onOpen("members", {server: response.data})
+        } catch (error) {
+            console.log(error)
+        } finally{
+            setLoadingId("")
+        }
+    }
+
     const onRoleChange = async (memberId: string, role: MemberRole) => {
         try {
             setLoadingId(memberId)
             const url = qs.stringifyUrl({url: `/api/members/${memberId}`,
         query: {
             serverId: server?.id,
-            memberId,
         }})
         const response = await axios.patch(url, {role})
         router.refresh()
@@ -121,7 +141,7 @@ export const MembersModal = () => {
                                         </DropdownMenuPortal>
                                     </DropdownMenuSub>
                                     <DropdownMenuSeparator />
-                                    <DropdownMenuItem>
+                                    <DropdownMenuItem onClick={()=>onKick(member.id)}>
                                         <Gavel className="h-4 w-4 mr-2" />
                                         Kick
                                     </DropdownMenuItem>
